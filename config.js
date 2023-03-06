a8e452b257d60c329d96e56d499a8687eee9b179
Fluke1232 <121549954+Fluke1232@users.noreply.github.com>
2023-03-06 14:34:53 +0300

    worlddestek

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "MTA3OTgwMjM5MDA0OTQ3NjY4OQ.GQAa6n.U3WGeUR540Ah1DlRV2mudTftanoxBeP-xk0MYE",
+    prefix: "w!",
+    content: "Destek Talebi Oluşturmak İçin Tİke Basınız!.",
+    channel: "1079468738203627553",
+    button: "🎗️",
+    yetkili: "1079468737201197153",
+    log: "1079802779251523675"
+}
